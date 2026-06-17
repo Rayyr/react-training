26964d6301044ece08d615c286fbdb726c61b060
raya <khasaatirayaa@gmail.com>
2026-06-18 01:04:00 +0300

    add filters to tasks

--- src/List1.jsx
+++ src/List1.jsx
@@ -4,44 +4,80 @@ import { useState } from "react";
 function List1() {
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
+  const [filter, setFilter] = useState("All");
 
   const handleAddClick = () => {
     if (task.trim() === "") return;
-    setTasks([...tasks, task]);
+
+    const newTask = {
+      id: Date.now(),
+      completed: false,
+      content: task,
+    };
+    setTasks([...tasks, newTask]);
     setTask("");
     return;
   };
 
-  
   function handleDeleteClick(indexToDelete) {
-    setTasks(tasks.filter((_, index) => index !== indexToDelete));
+    setTasks(tasks.filter((t) => t.id !== indexToDelete));
   }
+
+  function toggleStatus(id) {
+    const newTasks = tasks.map((task) =>
+      task.id === id
+        ? {
+            ...task,
+            completed: !task.completed,
+          }
+        : task 
+    );
+    setTasks(newTasks);
+    return;
+  }
+
+  const filteredTasks = tasks.filter((t) => {
+    if (filter === "Completed") return t.completed === true;
+    if (filter === "Active") return t.completed === false;
+    return true;
+  });
+
+
+  
 
   return (
     <div className="main-container">
       <h1>To Do List without localstorage</h1>
-     <div className="task-bar">
-      <form>
-        <input
-          type="text"
-          id="task-content"
-          style={{ width: "500px" }}
-          value={task}
-          onChange={(e) => {
-            setTask(e.target.value);
-          }}
-        />
-      </form>
+      <div className="task-bar">
+        <form>
+          <input
+            type="text"
+            id="task-content"
+            style={{ width: "500px" }}
+            value={task}
+            onChange={(e) => {
+              setTask(e.target.value);
+            }}
+          />
+        </form>
 
-      <button onClick={handleAddClick}>Add Task</button>
+        <button onClick={handleAddClick}>Add Task</button>
       </div>
 
-      
+      <div classNames="filters">
+        <button className={filter==="All"?"colored":"plain"} onClick={() => setFilter("All")}>All</button>
+        <button className={filter==="Completed"?"colored":"plain"} onClick={() => setFilter("Completed") }>Completed</button>
+        <button className={filter==="Active"?"colored":"plain"} onClick={() => setFilter("Active") }>Active</button>
+      </div>
+
       <div className="tasks">
-        {tasks.map((t, index) => (
-          <div className="added-task" key={index}>
-            <p>{t}</p>
-            <button onClick={()=>handleDeleteClick(index)}>Delete Task</button>
+        {filteredTasks.map((t) => (
+          <div className="added-task" key={t.id}>
+            <p>{t.content}</p>
+            <button onClick={() => toggleStatus(t.id)}>
+              {t.completed ? "Completed" : "Active"}
+            </button>
+            <button onClick={() => handleDeleteClick(t.id)}>Delete Task</button>
           </div>
         ))}
       </div>
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <List2 />       {/*support local storage*/}
+    <List1 />       {/*support local storage*/}
   {/*  <List1 />*/} {/*no local storage*/}
   </React.StrictMode>
 );
