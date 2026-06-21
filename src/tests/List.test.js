7d4bc058ce01c14d25677164b7824cb46b2ed2e3
raya <khasaatirayaa@gmail.com>
2026-06-22 01:25:03 +0300

    add test cases for filters  testing

--- src/tests/List.test.js
+++ src/tests/List.test.js
@@ -1,20 +1,57 @@
-import List2 from '../List2.jsx';
-import {render,screen} from '@testing-library/react';
-import "@testing-library/jest-dom"; //for matchers functions
+import App from "../App.jsx";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
 
-//test the title of the component
-test("test-title",()=>{
-    render(<List2/>);
-    const title=screen.getByTestId("title");//from testing/react docs
-    expect(title).toHaveTextContent("To Do List with localstorage");//matcher = from jest dom docs
+function addTask(taskName) {
+  const input = screen.getByRole("textbox");
+  const addBtn = screen.getByText("Add Task");
+
+  userEvent.type(input, taskName);
+  userEvent.click(addBtn);
+}
+
+test("All filter shows active and completed tasks", () => {
+  render(<App />);
+
+  addTask("Task 1");
+  addTask("Task 2");
+
+  const activeButtons = screen.getAllByText("Active");
+  userEvent.click(activeButtons[0]); // make Task 1 completed
+
+  userEvent.click(screen.getByText("All"));
+
+  expect(screen.getByText("Task 1")).toBeInTheDocument();
+  expect(screen.getByText("Task 2")).toBeInTheDocument();
 });
 
+test("Active filter shows only active tasks", () => {
+  render(<App />);
 
-//test if btn is existed in DOM
-test("test-btn-existence",()=>{
-    render(<List2/>);
-    const addTaskBtn=screen.getByText("Add Task");
-    expect(addTaskBtn).toBeInTheDocument();
- 
-})
+  addTask("Task 1");
+  addTask("Task 2");
 
+  const activeButtons = screen.getAllByText("Active");
+  userEvent.click(activeButtons[1]); //make Task 1 completed
+
+  userEvent.click(activeButtons[0]);//active btn
+
+  expect(screen.queryByText("Task 1")).not.toBeInTheDocument();
+  expect(screen.getByText("Task 2")).toBeInTheDocument();
+});
+
+test("Completed filter shows only completed tasks", () => {
+  render(<App />);
+
+  addTask("Task 1");
+  addTask("Task 2");
+
+  const activeButtons = screen.getAllByText("Active");
+  userEvent.click(activeButtons[1]); // make Task 1 completed
+
+  userEvent.click(screen.getAllByText("Completed")[0]);
+
+  expect(screen.getByText("Task 1")).toBeInTheDocument();
+  expect(screen.queryByText("Task 2")).not.toBeInTheDocument();
+});
