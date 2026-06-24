fa7c198bc10b001cb660db83faf06dcab4d04be2
raya <khasaatirayaa@gmail.com>
2026-06-25 00:44:03 +0300

    change the display of students into grid instead of flex by row

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import ListItem from "./ListItem";
 
-function StudentList({ onDeleteStudent,list, isBlocked }) {
+function StudentList({ onDeleteStudent, list, isBlocked }) {
   return (
     <div
       style={{
-        display: "flex",
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(260px, 1fr))",
         gap: "20px",
         padding: "20px",
         overflowX: "auto", // 🔥 allows horizontal scroll
       }}
     >
       {list.map((e, ind) => (
-        <ListItem onDeleteStudent={()=>onDeleteStudent(ind)} content={e} key={ind} isBlocked={isBlocked} />
+        <ListItem
+          onDeleteStudent={() => onDeleteStudent(ind)}
+          content={e}
+          key={ind}
+          isBlocked={isBlocked}
+        />
       ))}
     </div>
   );
