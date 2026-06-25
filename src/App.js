a4a1b20c03182be2ba6603f7f6282bf185846840
raya <khasaatirayaa@gmail.com>
2026-06-25 14:39:55 +0300

    add pages into project structure and add home page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
  import './App.css';
 import Dashboard from './components/Dashboard.jsx';
 import {BrowserRouter,Routes,Route,Link} from 'react-router-dom';
+import Home from './Pages/Home.jsx';
 
 function App() {
   return (
  
     <Routes>
-      <Route path="/"  element="Home.jsx"/>
+      <Route path="/"  element={<Home/>}/>
       <Route path="/students" element="Students.jsx"/>
       <Route path="/students/:id" element="StudentDetails.jsx"/>
       <Route path="/about" element="About.jsx"/>
-    <Dashboard/>
+      <Route path="/d" element={<Dashboard/>}/>
    </Routes>
   );
 }
