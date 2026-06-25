0875df3364aa5bbc3c947bec9a8d3929ac253a90
raya <khasaatirayaa@gmail.com>
2026-06-25 14:26:56 +0300

    set up react router and add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
  import './App.css';
 import Dashboard from './components/Dashboard.jsx';
+import {BrowserRouter,Routes,Route,Link} from 'react-router-dom';
+
 function App() {
   return (
+ 
+    <Routes>
+      <Route path="/"  element="Home.jsx"/>
+      <Route path="/students" element="Students.jsx"/>
+      <Route path="/students/:id" element="StudentDetails.jsx"/>
+      <Route path="/about" element="About.jsx"/>
     <Dashboard/>
+   </Routes>
   );
 }
 
