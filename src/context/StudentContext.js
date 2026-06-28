0f356253d7cd30f3b05ec30719506b62165a155c
raya <khasaatirayaa@gmail.com>
2026-06-28 13:33:37 +0300

    modify the add/remove Student functionality in StudentContext due to Async behavior of useState

--- src/context/StudentContext.js
+++ src/context/StudentContext.js
@@ -3,33 +3,28 @@ import { createContext, useState } from "react";
 export const StudentContext = createContext();
 
 export const StudentProvider = ({ children }) => {
-  const [students, setStudents] = useState(()=>{
-    const savedStudents=localStorage.getItem("students");
-    return savedStudents===null?[]:JSON.parse(savedStudents);  
+  //shared props
+  const [students, setStudents] = useState(() => {
+    const savedStudents = localStorage.getItem("students");
+    return savedStudents === null ? [] : JSON.parse(savedStudents);
   });
 
   // ✅ Add student
   const addStudent = (newStudent) => {
-    setStudents((prev) => [
-      ...prev,
-      newStudent
-    ]);
-    localStorage.setItem("students",JSON.stringify(students));
+    const newStudents = [...students, newStudent]; //since the setStsate does not make an immediate update on state
+    setStudents(newStudents);
+    localStorage.setItem("students", JSON.stringify(newStudents));
   };
 
   // ✅ Remove student
   const removeStudent = (indexToBeDelted) => {
-    setStudents((prev) =>
-      prev.filter((_,index) => index !== indexToBeDelted)
-    );
-        localStorage.setItem("students", JSON.stringify(students));
-
+    const newStudents = students.filter((_, i) => i !== indexToBeDelted);
+    setStudents(newStudents);
+    localStorage.setItem("students", JSON.stringify(newStudents));
   };
 
   return (
-    <StudentContext.Provider
-      value={{ students, addStudent, removeStudent }}
-    >
+    <StudentContext.Provider value={{ students, addStudent, removeStudent }}>
       {children}
     </StudentContext.Provider>
   );
